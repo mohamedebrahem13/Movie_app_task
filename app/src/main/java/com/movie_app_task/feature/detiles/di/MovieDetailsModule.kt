d60ac67be81c043e190cae7a96eace42d9029866
mohamedebrahem13 <mohamedebrahem1447@gmail.com>
2025-07-20 00:01:02 +0300

    feat: add movie details use cases  and add it to di module , create the mapper

--- app/src/main/java/com/movie_app_task/feature/detiles/di/MovieDetailsModule.kt
+++ app/src/main/java/com/movie_app_task/feature/detiles/di/MovieDetailsModule.kt
@@ -5,6 +5,7 @@ import com.movie_app_task.feature.detiles.data.repository.MovieDetailsRepository
 import com.movie_app_task.feature.detiles.data.repository.local.MovieDetailsLocalDataSourceImpl
 import com.movie_app_task.feature.detiles.domain.repository.MovieDetailsRepository
 import com.movie_app_task.feature.detiles.domain.repository.local.MovieDetailsLocalDataSource
+import com.movie_app_task.feature.detiles.domain.usecase.GetMovieDetailsByIdUseCase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -22,5 +23,9 @@ object MovieDetailsModule {
     fun provideMovieDetailsRepository(
         localDataSource: MovieDetailsLocalDataSource
     ): MovieDetailsRepository = MovieDetailsRepositoryImpl(localDataSource)
+    @Provides
+    fun provideGetMovieDetailsByIdUseCase(
+        repository: MovieDetailsRepository
+    ): GetMovieDetailsByIdUseCase = GetMovieDetailsByIdUseCase(repository)
 
 }
